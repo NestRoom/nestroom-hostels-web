4561b35f33327c40da4fd84921b3b6c85246bf45
Sourashis Ghosh Roy <personal.sourashisghoshroy@gmail.com>
2026-03-19 11:19:09 +0530

    feat: Add Logo component to the top of Sidebar

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -1,10 +1,16 @@
+import Link from 'next/link';
 import styles from './sidebar.module.css';
 
 export default function Sidebar() {
   return (
     <aside className={styles.sidebar}>
+      <div className={styles.logoContainer}>
+        <Link href="/" className={styles.logo}>
+          nest<span className={styles.blueText}>room</span>
+        </Link>
+      </div>
+
       {/* Sidebar navigation and layout will go here */}
-      Sidebar Component Placeholder
     </aside>
   );
 }
